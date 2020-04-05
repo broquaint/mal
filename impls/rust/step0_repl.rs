a9bacb1690198b172ceb915c227e8e14ad0fb9c3
Dan Brook <dan@broquaint.com>
2020-04-05 12:00:52 +0100

    Initial reboot of rust impl
    Got as far as having step0 passing its tests.

--- impls/rust/step0_repl.rs
+++ impls/rust/step0_repl.rs
@@ -1,31 +1,33 @@
-extern crate rustyline;
+use std::io::{self, Write};
 
-use rustyline::error::ReadlineError;
-use rustyline::Editor;
+#[allow(non_snake_case)]
+fn READ(code: String) -> String {
+    code
+}
+
+#[allow(non_snake_case)]
+fn EVAL(code: String) -> String {
+    code
+}
+
+#[allow(non_snake_case)]
+fn PRINT(code: String) -> String {
+    code
+}
+
+fn rep(code: String) -> String {
+    PRINT(READ(EVAL(code)))
+}
 
 fn main() {
-    // `()` can be used when no completer is required
-    let mut rl = Editor::<()>::new();
-    if rl.load_history(".mal-history").is_err() {
-        eprintln!("No previous history.");
-    }
-
     loop {
-        let readline = rl.readline("user> ");
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(&line);
-                rl.save_history(".mal-history").unwrap();
-                if line.len() > 0 {
-                    println!("{}", line);
-                }
-            }
-            Err(ReadlineError::Interrupted) => continue,
-            Err(ReadlineError::Eof) => break,
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
+        print!("user> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_)  => { println!("{}", rep(input)); }
+            Err(_) => { break; }
         }
     }
 }
--- impls/rust/step1_read_print.rs
+++ impls/rust/step1_read_print.rs
@@ -1,51 +1,14 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate fnv;
-extern crate itertools;
-extern crate regex;
-
-extern crate rustyline;
-use rustyline::error::ReadlineError;
-use rustyline::Editor;
-
-#[macro_use]
-#[allow(dead_code)]
-mod types;
-use crate::types::format_error;
-mod printer;
-mod reader;
-// TODO: figure out a way to avoid including env
-#[allow(dead_code)]
-mod env;
+use std::io;
 
 fn main() {
-    // `()` can be used when no completer is required
-    let mut rl = Editor::<()>::new();
-    if rl.load_history(".mal-history").is_err() {
-        eprintln!("No previous history.");
-    }
-
     loop {
-        let readline = rl.readline("user> ");
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(&line);
-                rl.save_history(".mal-history").unwrap();
-                if line.len() > 0 {
-                    match reader::read_str(line) {
-                        Ok(mv) => {
-                            println!("{}", mv.pr_str(true));
-                        }
-                        Err(e) => println!("Error: {}", format_error(e)),
-                    }
-                }
+        println!("user> ");
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                println!("{}", input);
             }
-            Err(ReadlineError::Interrupted) => continue,
-            Err(ReadlineError::Eof) => break,
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
+            Err(error) => println!("error: {}", error),
         }
     }
 }
