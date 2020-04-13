68b682ec9ac85039947231b7f3f7aa5dd75a04c0
Dan Brook <dan@broquaint.com>
2020-04-13 12:38:51 +0100

    Step 4 implement user defined function
    Managed to get this running with hardly any borrowing etc faff which
    is a pleasant surprise. The fn* tests largely pass but fail hard on
    variable length args.

--- impls/rust/env.rs
+++ impls/rust/env.rs
@@ -1,6 +1,7 @@
+use std::rc::Rc;
 use std::collections::HashMap;
 
-use types::MalVal;
+use types::MalVal::{self, Sym};
 
 #[derive(Clone)]
 pub struct MalEnv {
@@ -11,6 +12,20 @@ pub struct MalEnv {
 impl MalEnv {
     pub fn make_inner(&self) -> MalEnv {
         MalEnv { outer: Some(Box::new(self.clone())), data: HashMap::new() }
+    }
+
+    pub fn make_inner_with(&self, binds: &Rc<Vec<MalVal>>, args: &[MalVal]) -> Result<MalEnv, String> {
+        let mut params = HashMap::new();
+
+        for idx in 0 .. binds.len() {
+            match &binds[idx] {
+                // TODO Result!
+                Sym(s) => { params.insert(s.clone(), args[idx].clone()); }
+                _ => return Err("Got a non-sym in fn*".to_string())
+            }
+        }
+
+        Ok(MalEnv { outer: Some(Box::new(self.clone())), data: params })
     }
 
     pub fn set(&mut self, k: String, v: MalVal) {
