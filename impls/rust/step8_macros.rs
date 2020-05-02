e061af499e3da6ad2be8d0d742596b45135d1a6d
Dan Brook <dan@broquaint.com>
2020-05-02 10:52:20 +0100

    Step 8 initial commit unchanged from step 7

---
+++ impls/rust/step8_macros.rs
@@ -0,0 +1,291 @@
+use std::env::args;
+use std::io::{self, Write};
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::cell::Cell;
+use std::cell::RefCell;
+
+extern crate regex;
+
+mod types;
+use types::MalVal::{self, *};
+use types::MalUserFn;
+mod reader;
+use reader::read_str;
+use reader::mal_sym;
+mod printer;
+use printer::pr_str;
+mod env;
+use env::MalEnv;
+mod core;
+use core::core_ns; // Also exports err!, mlist! macros.
+use core::call_user_fun;
+use core::MalRet;
+
+fn eval_ast(ast: &MalVal, menv: &Rc<MalEnv>) -> MalRet {
+//    println!("eval_ast: {}", pr_str(ast.clone(), true));
+    match ast {
+        List(l) => {
+            let mut new_list: Vec<MalVal> = Vec::new();
+            // Iterate with for-loop to raise the first Err encountered.
+            for v in l.iter() {
+                new_list.push(EVAL(&v, menv)?);
+            }
+            Ok(as_mal_list!(new_list))
+        },
+        Vector(l) => {
+            let mut new_vec: Vec<MalVal> = Vec::new();
+            // Iterate with for-loop to raise the first Err encountered.
+            for v in l.iter() {
+                new_vec.push(EVAL(v, menv)?);
+            }
+            Ok(Vector(Rc::new(new_vec)))
+        },
+        Map(l) => {
+            let mut new_map: HashMap<String, MalVal> = HashMap::new();
+            // Iterate with for-loop to raise the first Err encountered.
+            for (k, v) in l.iter() {
+                new_map.insert(k.clone(), EVAL(v, menv)?);
+            }
+            Ok(Map(Rc::new(new_map)))
+        },
+        Sym(s) => menv.get(&s),
+        _ => Ok(ast.clone())
+    }
+}
+
+fn is_true(cond: MalVal) -> bool {
+    match cond {
+        Bool(b) => b,
+        Nil     => false,
+        _       => true
+    }
+}
+
+fn make_env(binds: &Rc<Vec<MalVal>>, outer_env: &Rc<MalEnv>) -> Result<Rc<MalEnv>, String> {
+    let new_env = Rc::new(MalEnv {
+        outer: Some(Rc::clone(outer_env)),
+        data:  Rc::new(RefCell::new(HashMap::new())),
+        id:    outer_env.id * 2,
+    });
+
+    if binds.len() % 2 != 0 {
+        return err!("binds for let* wasn't even")
+    }
+
+    for bind in binds.chunks(2) {
+        match &bind[0] {
+            Sym(k) => {
+                let v = EVAL(&bind[1], &new_env)?;
+                new_env.set(k.clone(), v);
+            }
+            _ => return err!("bind in let* wasn't a symbol")
+        }
+    }
+
+    Ok(new_env)
+}
+
+fn quasiquote(ast: MalVal) -> MalVal {
+    match ast {
+        List(l) | Vector(l) if !l.is_empty() => {
+            let(head, tail) = l.split_first().unwrap();
+            match head {
+                Sym(s) if s == "unquote" => return tail[0].clone(),
+                List(l) | Vector(l) => {
+                    // A chained if-let would make this a bit cleaner.
+                    if let Sym(s) = &l[0] {
+                        if s == "splice-unquote" {
+                            let(_, ht) = l.split_first().unwrap();
+                            return mal_list![
+                                mal_sym("concat"), ht[0].clone(), quasiquote(as_mal_list![tail.to_vec()])
+                            ]
+                        }
+                    }
+                }
+                _ => { /* This would all be nicer with chained if-let */ }
+            }
+            mal_list![mal_sym("cons"), quasiquote(head.clone()), quasiquote(as_mal_list![tail.to_vec()])]
+        }
+        _ => mal_list![mal_sym("quote"), ast]
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn EVAL(cur_ast: &MalVal, cur_env: &Rc<MalEnv>) -> MalRet {
+//    println!("    EVAL: {}", pr_str(cur_ast.clone(), true));
+
+    let ast = Cell::new(cur_ast);
+    let env = RefCell::new(Rc::clone(cur_env));
+
+    'eval_loop: loop {
+        match ast.get() {
+            List(l) => {
+                if l.is_empty() {
+                    return Ok(ast.get().clone())
+                }
+                else {
+                    // Shouldn't panic as we know l isn't empty.
+                    let (sym, rest) = l.split_first().unwrap();
+                    if let Sym(tok) = sym {
+                        match tok.as_str() {
+                            "def!" => {
+                                let (name, args) = rest.split_first().unwrap();
+                                return match name {
+                                    Sym(s) => {
+                                        let val = EVAL(&args[0], &env.borrow())?;
+                                        env.borrow().set(s.clone(), val.clone());
+                                        Ok(val)
+                                    },
+                                    _ => err!("First elem of def! wasn't a Sym")
+                                }
+                            }
+                            "let*" => {
+                                let binds = &rest[0];
+                                let form = &rest[1];
+                                match binds {
+                                    List(bl) | Vector(bl) => {
+                                        let new_env = make_env(bl, &env.borrow())?;
+                                        env.replace(new_env);
+                                        ast.replace(form);
+                                        continue 'eval_loop
+                                    }
+                                    _ => return err!("First elem of let* wasn't a list/vec")
+                                }
+                            }
+                            "do" => {
+                                let(last, leading) = rest.split_last().unwrap();
+                                eval_ast(&as_mal_list![leading.to_vec()], &env.borrow())?;
+                                ast.replace(&last);
+                                continue 'eval_loop
+                            }
+                            "if" => {
+                                // rest[0] = cond, rest[1] = true branch, rest[2] = false branch
+                                let next_ast = if is_true(EVAL(&rest[0], &env.borrow())?) {
+                                    &rest[1]
+                                }
+                                else if rest.len() > 2 {
+                                    &rest[2]
+                                }
+                                else {
+                                   &Nil
+                                };
+                                ast.replace(next_ast);
+                                continue 'eval_loop
+                            }
+                            "fn*" => {
+                                let binds = &rest[0];
+                                let body  = &rest[1];
+                                return match binds {
+                                    List(bl) | Vector(bl) => {
+                                        Ok(
+                                            UserFun(
+                                                MalUserFn {
+                                                    binds: bl.clone(),
+                                                    body:  Rc::new(body.clone()),
+                                                    env:   Rc::clone(&env.borrow()),
+                                                    // Allow calling user functions in core.
+                                                    eval:  EVAL
+                                                }
+                                            )
+                                        )
+                                    }
+                                    _ => err!("First elem of fn* wasn't a list/vec")
+                                }
+                            }
+                            "eval" => {
+                                // Get the ast to run EVAL against.
+                                let tmp_ast = EVAL(&rest[0], &env.borrow())?;
+                                // Evaluate the produced AST.
+                                return EVAL(&tmp_ast, &env.borrow().root());
+                            }
+                            "quote" => {
+                                return Ok(rest[0].clone());
+                            }
+                            "quasiquote" => {
+                                let quoted = quasiquote(rest[0].clone());
+                                return EVAL(&quoted, &env.borrow());
+                            }
+                            _ => { /* fallthrough to function call */ }
+                        }
+                    }
+
+                    return if let List(fcall) = eval_ast(ast.get(), &env.borrow())? {
+                        let (fun, args) = fcall.split_first().unwrap();
+                        match fun {
+                            CoreFun(f) => f(args),
+                            UserFun(f) => call_user_fun(f, args),
+                            _ => err!("Tried to call function on non-Fun")
+                        }
+                    }
+                    else {
+                        err!("Somehow eval_ast(List) didn't return a list?")
+                    }
+                }
+            }
+            v => return eval_ast(&v, &env.borrow())
+        }
+    }
+}
+
+#[allow(non_snake_case)]
+fn PRINT(code: &MalVal) -> String {
+    pr_str(code.clone(), true) // TODO Don't be so lazy with clone.
+}
+
+#[allow(non_snake_case)]
+fn READ(code: String) -> MalRet {
+    read_str(code)
+}
+
+fn rep(code: String, menv: &Rc<MalEnv>) -> Result<String, String> {
+    let ast = READ(code)?;
+    match EVAL(&ast, menv) {
+        Ok(ast) => Ok(PRINT(&ast)),
+        Err(e) => Err(e)
+    }
+}
+
+fn rep_lit(code: &str, env: &Rc<MalEnv>) {
+    if let Err(e) = rep(code.to_string(), env) {
+       println!("Failed to eval '{}': {}", code, e);
+       std::process::exit(1);
+   }
+}
+
+fn main() {
+    let repl_env = Rc::new(MalEnv { outer: None, data: Rc::new(RefCell::new(HashMap::new())), id: 1 });
+
+    for (k,v) in &core_ns() {
+        repl_env.set(k.clone(), v.clone());
+    }
+
+    rep_lit("(def! not (fn* (a) (if a false true)))", &repl_env);
+    rep_lit("(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \"\nnil)\")))))", &repl_env);
+
+    let argstar  = "*ARGV*".to_string();
+    let mut argv = args();
+    if argv.len() > 1 {
+        let file = argv.next().unwrap();
+        repl_env.set(
+            argstar, as_mal_list![argv.map(|arg| Str(arg)).collect()]
+        );
+        rep_lit(format!("(load-file {})", file).as_str(), &repl_env)
+    }
+    else {
+        repl_env.set(argstar, mal_list![]);
+        loop {
+            print!("user> ");
+            io::stdout().flush().unwrap();
+
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(_)  => {
+                    let result = rep(input, &repl_env);
+                    println!("{}", match result { Ok(s) => s, Err(e) => e });
+                }
+                Err(_) => { break; }
+            }
+        }
+    }
+}
