5600561e913e8ec74f0caf0e56c9a87a4294f7ef
Dan Brook <dan@broquaint.com>
2020-05-17 23:38:00 +0100

    Have env related functions callable on Rc<MalEnv>
    Makes the code a bit cleaner which is nice.

--- impls/rust/env.rs
+++ impls/rust/env.rs
@@ -7,6 +7,7 @@ use types::MalVal::{self, *};
 use types::MalErr;
 use types::MalRet;
 use types::VecLike;
+use types::EvalSig;
 
 use printer::pr_str;
 use printer::malstr_as_string;
@@ -24,55 +25,15 @@ pub struct MalEnv {
 }
 
 impl MalEnv {
-    pub fn make(outer: Option<Rc<MalEnv>>, params: HashMap<String, MalVal>) -> Rc<MalEnv> {
+    fn new(outer: Option<Rc<MalEnv>>, params: HashMap<String, MalVal>) -> Rc<MalEnv> {
         Rc::new(MalEnv {
             outer: outer,
             data:  Rc::new(RefCell::new(params)),
         })
     }
 
-    pub fn make_root() -> Rc<MalEnv> {
-        MalEnv::make(None, HashMap::new())
-    }
-
-    pub fn make_inner(outer_env: &Rc<MalEnv>) -> Rc<MalEnv> {
-        MalEnv::make(Some(Rc::clone(outer_env)), HashMap::new())
-    }
-
-    pub fn make_bound(outer_env: &Rc<MalEnv>, params: HashMap<String, MalVal>) -> Rc<MalEnv> {
-        MalEnv::make(Some(Rc::clone(outer_env)), params)
-    }
-
-    pub fn make_bound_env(env: &Rc<MalEnv>, binds: &Box<VecLike>, args: &[MalVal]) -> Result<Rc<MalEnv>, MalErr> {
-        let mut params = HashMap::new();
-    
-        for idx in 0 .. binds.len() {
-            match &binds[idx] {
-                Sym(s) => {
-                    if s == "&" {
-                        if let Sym(rest_bind) = &binds[idx + 1] {
-                            let rest_args = &args[idx .. args.len()];
-                            params.insert(rest_bind.clone(), as_mal_list!(rest_args.to_vec()));
-                            break;
-                        }
-                        else {
-                            return err!("Got a non-sym after & in binds")
-                        }
-                    }
-                    else {
-                        if idx < args.len() {
-                            params.insert(s.clone(), args[idx].clone());
-                        }
-                        else {
-                            return errf!("have {} binds but got {} args", binds.len(), args.len());
-                        }
-                    }
-                }
-                _ => return err!("Got a non-sym in fn*")
-            }
-        }
-    
-        Ok(MalEnv::make_bound(env, params))
+    pub fn new_root() -> Rc<MalEnv> {
+        MalEnv::new(None, HashMap::new())
     }
 
     pub fn set(&self, k: String, v: MalVal) {
@@ -118,5 +79,67 @@ impl ToString for MalEnv {
                 .filter(|(_,v)| !matches!(v, CoreFun(_)))
                 .map(|(k,v)| format!("  {} {}\n", malstr_as_string(k.clone(), true), v_to_str!(v)))
                 .collect::<Vec<String>>().join(" "))
+    }
+}
+
+type MalEnvRet = Result<Rc<MalEnv>, MalErr>;
+
+pub trait MakeRcEnv {
+    fn bound_to(&self, binds: &Box<VecLike>, args: &[MalVal]) -> MalEnvRet;
+
+    fn for_let(&self, binds: &VecLike, eval: EvalSig) -> MalEnvRet;
+}
+
+impl MakeRcEnv for Rc<MalEnv> {
+    fn bound_to(&self, binds: &Box<VecLike>, args: &[MalVal]) -> MalEnvRet {
+        let mut params = HashMap::new();
+
+        for idx in 0 .. binds.len() {
+            match &binds[idx] {
+                Sym(s) => {
+                    if s == "&" {
+                        if let Sym(rest_bind) = &binds[idx + 1] {
+                            let rest_args = &args[idx .. args.len()];
+                            params.insert(rest_bind.clone(), as_mal_list!(rest_args.to_vec()));
+                            break;
+                        }
+                        else {
+                            return err!("Got a non-sym after & in binds")
+                        }
+                    }
+                    else {
+                        if idx < args.len() {
+                            params.insert(s.clone(), args[idx].clone());
+                        }
+                        else {
+                            return errf!("have {} binds but got {} args", binds.len(), args.len());
+                        }
+                    }
+                }
+                _ => return err!("Got a non-sym in fn*")
+            }
+        }
+
+        Ok(MalEnv::new(Some(Rc::clone(self)), params))
+    }
+
+    fn for_let(&self, binds: &VecLike, eval: EvalSig) -> MalEnvRet {
+        let new_env = MalEnv::new(Some(Rc::clone(self)), HashMap::new());
+
+        if binds.len() % 2 != 0 {
+            return err!("binds for let* wasn't even")
+        }
+
+        for bind in binds.chunks(2) {
+            match &bind[0] {
+                Sym(k) => {
+                    let v = eval(Rc::new(bind[1].clone()), Rc::clone(&new_env))?;
+                    new_env.set(k.clone(), v);
+                }
+                _ => return err!("bind in let* wasn't a symbol")
+            }
+        }
+
+        Ok(new_env)
     }
 }
