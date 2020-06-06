45d51c9252118e9cb4f890e6850763b08adc4cfb
Dan Brook <dan@broquaint.com>
2020-06-06 18:25:07 +0100

    Step 1 some tests passing
    1. Code mostly works after some dumb compile fails \o/
    2. Previously passing because it was just running step 0, now running
    reader code and passing some. Next is multiple elements in a list.

--- impls/erlang/src/reader.erl
+++ impls/erlang/src/reader.erl
@@ -1,1 +1,71 @@
 -module(reader).
+
+-export([read_str/1]).
+
+tokenize(Code) ->
+    MalSyntaxRe = "[\\s,]*(~@|[\\[\\]{}()'`~^@]|\x22(?:\\.|[^\\\x22])*\x22?|;.*|[^\\s\[\\]{}('\x22`,;)]+)",
+    % run(Subject, RE, Options) ->
+    %   {match, Captured} | match | nomatch | {error, ErrType}
+    case re:run(Code, MalSyntaxRe, [global]) of
+        {match, Captured} -> 
+            Tokens = [lists:sublist(Code, Pos + 1, Len) || [_, {Pos, Len}] <- Captured],
+
+            {success, Tokens};
+        nomatch -> {error, "Failed to match"}
+        % As this is a global match and a valid RE we shouldn't encounter these.
+        % match | {error, ErrType} -> 
+        end.
+
+%% r_peek(Reader) ->
+%%     lists:nth(maps:get(pos, Reader), maps:get(tokens, Reader)).
+
+%% r_next(Reader) ->
+%%     CurPos = maps:get(pos, Reader),
+%%     Token  = lists:nth(CurPos, maps:get(tokens, Reader)),
+%%     {Token, maps:update(pos, CurPos + 1, Reader)}.
+
+% -record(mal_list, {elems}).
+% -record(mal_scalar, {val}).
+
+%% mal_list_append(List, Elem) ->
+%%     mal_list_append(List, [Elem]).
+%% mal_list_append(List, [Elem]) ->
+%%     CurElems = List#mal_list{elems},
+%%     List#mal_list{elems = CurElems ++ Elem}.
+
+read_seq([]) -> {[],[]};
+read_seq([H|T]) -> read_seq([H|T], []).
+
+read_seq([], List) -> {List, []};
+%read_seq([H|Tail], List) when H == ")" -> {List, Tail};
+read_seq(Tokens, List) ->
+    [H|Tail] = Tokens,
+    case H of
+        ")" -> {List, Tail};
+        _   -> 
+            {Elem, Rest} = read_form(Tokens),
+            read_seq(Rest, List ++ [Elem])
+    end.
+
+read_atom([H|Tail]) ->
+    {H, Tail}.
+
+% Everything under read_form should return {List*, Tokens}
+read_form([]) -> [];
+read_form(Tokens) ->
+    [H|Tail] = Tokens,
+%    io:format("now at ~p~n", [r_peek(Reader)]),
+    case H of
+        "(" -> read_seq(Tail);
+        %% "[" => read_vec(r);
+        %% "{" => read_map(r);
+        _Else -> read_atom(Tokens)
+    end.
+
+read_str(Code) ->
+    case tokenize(Code) of
+        {success, Reader} -> 
+            {Ast, _} = read_form(Reader),
+            Ast;
+        {error, Err} -> Err
+        end.
--- impls/erlang/src/step1_read_print.erl
+++ impls/erlang/src/step1_read_print.erl
@@ -1,1 +1,27 @@
 -module(step1_read_print).
+
+-export([main/1]).
+
+main(_) ->
+    case io:get_line(standard_io, "user> ") of
+        eof ->
+            io:format("~n"),
+            ok;
+        {error, ErrorDescription} ->
+            io:format("Error reading input: ~p~n", [ErrorDescription]),
+            exit(ioerr);
+        Input ->
+            io:format("~s~n", [print(eval(read(Input)))]),
+            main("")
+        end.
+
+read(Code) ->
+    % TODO Errors!
+    reader:read_str(Code).
+
+eval(Code) ->
+    Code.
+
+print(Code) ->
+    Code.
+
