c3dad964d30864a31f48e9f6019e7ffb8e10a4cd
Dan Brook <dan@broquaint.com>
2020-06-06 18:25:07 +0100

    WIP Step 1 some tests passing
    1. Code mostly works after some dumb compile fails \o/
    2. Previously passing because it was just running step 0, now running
    reader code and passing some. Next is multiple elements in a list.

--- impls/erlang/src/reader.erl
+++ impls/erlang/src/reader.erl
@@ -1,1 +1,58 @@
 -module(reader).
+
+-export([read_str/1]).
+
+tokenize(Code) ->
+    MalSyntaxRe = "[\\s,]*(~@|[\\[\\]{}()'`~^@]|\x22(?:\\.|[^\\\x22])*\x22?|;.*|[^\\s\[\\]{}('\x22`,;)]+)",
+    % run(Subject, RE, Options) ->
+    %   {match, Captured} | match | nomatch | {error, ErrType}
+    case re:run(Code, MalSyntaxRe, [global]) of
+        {match, Captured} -> 
+            Tokens = [lists:sublist(Code, Pos + 1, Len) || [_, {Pos, Len}] <- Captured],
+
+            {success, #{tokens=>Tokens,pos=>1}};
+        nomatch -> {error, "Failed to match"}
+        % As this is a global match and a valid RE we shouldn't encounter these.
+        % match | {error, ErrType} -> 
+        end.
+
+r_peek(Reader) ->
+    lists:nth(maps:get(pos, Reader), maps:get(tokens, Reader)).
+
+r_next(Reader) ->
+    CurPos = maps:get(pos, Reader),
+    Token  = lists:nth(CurPos, maps:get(tokens, Reader)),
+    {Token, maps:update(pos, CurPos + 1, Reader)}.
+
+%r_before_end(Reader) ->
+%    maps:get(pos, Reader) < length(maps:get(tokens, Reader)).
+
+read_seq(Reader) ->
+    {_, RBegin} = r_next(Reader), % Move past opening paren.
+    read_seq(RBegin, []).
+
+read_seq(Reader, List) ->
+    case r_peek(Reader) of
+        ")" -> {_, REnd} = r_next(Reader),
+               {List, REnd};
+        _   -> {Element, RNext} = read_form(Reader),
+               {List ++ [Element], RNext}
+    end.
+
+read_atom(Reader) ->
+    r_next(Reader).
+
+read_form(Reader) ->
+%    io:format("now at ~p~n", [r_peek(Reader)]),
+    case r_peek(Reader) of
+        "(" -> read_seq(Reader);
+        %% "[" => read_vec(r);
+        %% "{" => read_map(r);
+        _Else -> read_atom(Reader)
+    end.
+
+read_str(Code) ->
+    case tokenize(Code) of
+        {success, Reader} -> read_form(Reader);
+        {error, Err} -> Err
+        end.
--- impls/erlang/src/step1_read_print.erl
+++ impls/erlang/src/step1_read_print.erl
@@ -1,1 +1,28 @@
 -module(step1_read_print).
+
+-export([main/1]).
+
+main(_) ->
+    case io:get_line(standard_io, "user> ") of
+        eof ->
+            io:format("~n"),
+            ok;
+        {error, ErrorDescription} ->
+            io:format("Error reading input: ~p~n", [ErrorDescription]),
+            exit(ioerr);
+        Input ->
+            {Ast, _} = print(eval(read(Input))),
+            io:format("~s~n", [Ast]),
+            main("")
+        end.
+
+read(Code) ->
+    % TODO Errors!
+    reader:read_str(Code).
+
+eval(Code) ->
+    Code.
+
+print(Code) ->
+    Code.
+
