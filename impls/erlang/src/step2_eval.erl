3ed2d3e54eddda2d68ff71e6b00d26fb116b9ba4
Dan Brook <dan@broquaint.com>
2020-06-12 21:54:32 +0100

    Step 2 initial commit, straight copy from step 1

--- impls/erlang/src/step2_eval.erl
+++ impls/erlang/src/step2_eval.erl
@@ -1,1 +1,33 @@
 -module(step2_eval).
+
+-export([main/1]).
+
+main(_) -> repl().
+
+repl() ->
+    case io:get_line(standard_io, "user> ") of
+        eof ->
+            io:format("~n"),
+            ok;
+        {error, ErrorDescription} ->
+            io:format("Error reading input: ~p~n", [ErrorDescription]),
+            exit(ioerr);
+        Input ->
+            case read(Input) of
+                {success, Ast} ->
+                    io:format("~s~n", [print(eval(Ast))]);
+                {error, Err} -> io:format("Error: ~s~n", [Err])
+            end,
+            repl()
+        end.
+
+read(Code) ->
+    % TODO Errors!
+    reader:read_str(Code).
+
+eval(Code) ->
+    Code.
+
+print(Ast) ->
+    printer:pr_str(Ast).
+
