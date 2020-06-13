bd85b841a92a67cf019db483fb13c7b6ec60ba81
Dan Brook <dan@broquaint.com>
2020-06-13 10:53:04 +0100

    Step 2 functional implementation of eval
    But currently fails hard on errors.

--- impls/erlang/src/step2_eval.erl
+++ impls/erlang/src/step2_eval.erl
@@ -1,10 +1,21 @@
 -module(step2_eval).
 
+-include("types.hrl").
+
 -export([main/1]).
 
-main(_) -> repl().
+main(_) ->
+    ReplEnv = #{"+" => fun([A,B]) -> num(num(A) + num(B)) end,
+                "-" => fun([A,B]) -> num(num(A) - num(B)) end,
+                "*" => fun([A,B]) -> num(num(A) * num(B)) end,
+                "/" => fun([A,B]) -> num(trunc(num(A) / num(B))) end},
 
-repl() ->
+    repl(ReplEnv).
+
+num(#mal_num{val=V}) -> V;
+num(V) when is_integer(V) -> #mal_num{val=V}.
+
+repl(Env) ->
     case io:get_line(standard_io, "user> ") of
         eof ->
             io:format("~n"),
@@ -15,19 +26,34 @@ repl() ->
         Input ->
             case read(Input) of
                 {success, Ast} ->
-                    io:format("~s~n", [print(eval(Ast))]);
-                {error, Err} -> io:format("Error: ~s~n", [Err])
-            end,
-            repl()
+                    {Res, NextEnv} = eval(Ast, Env),
+                    io:format("~s~n", [print(Res)]),
+                    repl(NextEnv);
+                {error, Err} ->
+                    io:format("Error: ~s~n", [Err]),
+                    repl(Env)
+            end
         end.
 
 read(Code) ->
-    % TODO Errors!
     reader:read_str(Code).
-
-eval(Code) ->
-    Code.
 
 print(Ast) ->
     printer:pr_str(Ast).
 
+eval([], Env) -> {[], Env};
+eval(Ast, Env) when not is_record(Ast, mal_list) -> eval_ast(Ast, Env);
+eval(Ast, Env) ->
+    {#mal_list{elems=[F|Args]}, NextEnv} = eval_ast(Ast, Env),
+%    io:format("> applying ~p with ~p~n", [F, Args]),
+    {F(Args), NextEnv}.
+
+eval_list_elem(Ast, {List, Env}) ->
+    {Res, NextEnv} = eval(Ast, Env),
+    {List ++ [Res], NextEnv}.
+
+eval_ast(Ast, Env) when is_record(Ast, mal_list) ->
+    {E, NextEnv} = lists:foldl(fun eval_list_elem/2, {[], Env}, Ast#mal_list.elems),
+    {#mal_list{elems=E}, NextEnv};
+eval_ast(Ast, Env) when is_record(Ast, mal_sym) -> {map_get(Ast#mal_sym.val, Env), Env};
+eval_ast(Ast, Env) -> {Ast, Env}.
