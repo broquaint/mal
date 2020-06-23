53b79ceecef25d9f59b2f4ef447d745890341294
Dan Brook <dan@broquaint.com>
2020-06-23 23:02:23 +0100

    Step 4 Implement stateful environment
    Not entirely working but at least def! works to some degree. Using the
    simplest possible thing for the state, may need something more
    sophisticated in time.

--- impls/erlang/src/env.erl
+++ impls/erlang/src/env.erl
@@ -2,27 +2,87 @@
 
 -include("types.hrl").
 
--export([set/3, find/2, get/2, new/1]).
+-export([set/2, get/1, get/2, current/0, with/3, bound/1, run/1, start/1]).
 
-set(K, V, Env) ->
-    #mal_env{outer=Env#mal_env.outer, data=maps:put(K, V, Env#mal_env.data)}.
-
-find(K, Env) when Env#mal_env.outer =:= root ->
+e_find(K, Env) when Env#mal_env.outer =:= root ->
     case maps:find(K, Env#mal_env.data) of
         {ok, _} -> {ok, Env};
         error -> error
     end;
-find(K, Env) ->
+e_find(K, Env) ->
     case maps:find(K, Env#mal_env.data) of
         {ok, _} -> {ok, Env};
-        error -> find(K, Env#mal_env.outer)
+        error -> e_find(K, Env#mal_env.outer)
+    end.
+
+e_get(K, Env) ->
+    case e_find(K, Env) of
+        {ok, ResEnv} -> {ok, maps:get(K, ResEnv#mal_env.data)};
+        error -> {malerr, io_lib:format("Symbol '~s' not found in env", [K#mal_sym.val])}
+    end.
+
+with(K, V, Env) ->
+    #mal_env{outer=Env#mal_env.outer, data=maps:put(K, V, Env#mal_env.data)}.
+
+bound(Env) ->
+    #mal_env{outer=current(), data=Env#mal_env.data}.
+
+run(Env) ->
+    receive
+        {From, {set, K, V}} ->
+            Res = with(K, V, Env),
+            From ! ok,
+            run(Res);
+        {From, {get, K}} ->
+%            io:format("Looking for ~p in ~p~n", [K, Env]),
+            From ! e_get(K, Env),
+            run(Env);
+        {From, {current}} ->
+            From ! {ok, Env},
+            run(Env);
+        E ->
+            io:format("env:run fail: [~p]~n", [E]),
+            throw(mal_env_err)
+    end.
+
+set(K, V) ->
+    Pid = whereis(mal_env),
+    Pid ! {self(), {set, K, V}},
+    receive
+        ok -> ok
+    after 100 ->
+        throw(env_set_timeout)
+    end.
+
+get(K) ->
+    Pid = whereis(mal_env),
+%    io:format("Getting ~p in ~p~n", [K, Pid]),
+    Pid ! {self(), {get, K}},
+    receive
+        {ok, V} -> V;
+        {malerr, E} -> throw({malerr, E})
+    after 100 ->
+        throw(env_get_timeout)
     end.
 
 get(K, Env) ->
-    case find(K, Env) of
-        {ok, Env} -> maps:get(K, Env#mal_env.data);
-        error -> throw({malerr, io_lib:format("Symbol '~s' not found in env", [K#mal_sym.val])})
+    case e_get(K, Env) of
+        {ok, V} -> V;
+        {malerr, E} -> throw({malerr, E})
     end.
 
-new(M) ->
-    #mal_env{outer=root, data=M}.
+current() ->
+    Pid = whereis(mal_env),
+%    io:format("current env is ~p~n", [Pid]),
+    Pid ! {self(), {current}},
+    receive
+        {ok, Env} -> Env
+    after 100 ->
+        throw(env_current_timeout)
+    end.
+
+
+% new() -> new(#{}).
+start(M) ->
+    Pid = spawn(?MODULE, run, [#mal_env{outer=root, data=M}]),
+    register(mal_env, Pid).
--- impls/erlang/src/step4_if_fn_do.erl
+++ impls/erlang/src/step4_if_fn_do.erl
@@ -5,11 +5,10 @@
 -export([main/1]).
 
 main(_) ->
-    ReplEnv = env:new(core:ns()),
-    repl(ReplEnv).
+    env:start(core:ns()),
+    repl().
 
-
-repl(Env) ->
+repl() ->
     case io:get_line(standard_io, "user> ") of
         eof ->
             io:format("~n"),
@@ -20,18 +19,18 @@ repl(Env) ->
         Input ->
             case read(Input) of
                 {success, Ast} ->
-                    try eval(Ast, Env)
-                             of {Res, NextEnv} -> io:format("~s~n", [print(Res)]),
-                                                  repl(NextEnv)
+                    try eval(Ast, env:current())
+                             of Res -> io:format("~s~n", [print(Res)]),
+                                       repl()
                     catch
                         _:{malerr, Err} -> io:format("Exception: ~s~n", [Err]),
-                                         repl(Env);
+                                         repl();
                         _:Reason:Stack -> io:format("Mal bug: ~p~n~p~n", [Reason, Stack]),
-                                        repl(Env)
+                                        repl()
                     end;
                 {error, Err} ->
                     io:format("Reader error: ~s~n", [Err]),
-                    repl(Env)
+                    repl()
             end
         end.
 
@@ -43,35 +42,42 @@ print(Ast) ->
 
 eval(Ast, Env) when not is_record(Ast, mal_list) -> eval_ast(Ast, Env);
 eval(#mal_list{elems=[]}, Env) -> {#mal_list{elems=[]}, Env};
-eval(AstRec, CurEnv) ->
+eval(AstRec, Env) ->
     AstList = AstRec#mal_list.elems,
     [H|Tail] = AstList,
+%    io:format("ast is: ~p~n", [AstRec]),
+%    io:format("env is: ~p~n", [Env]),
     case H of
         #mal_sym{val="def!"} ->
             [K, E] = Tail,
-            {V, Env} = eval(E, CurEnv),
-            {V, env:set(K, V, Env)};
+            V = eval(E, Env), % Bug! recursive fail?
+            env:set(K, V),
+            V;
         #mal_sym{val="let*"} ->
             [Binds, Body] = Tail,
-            {Res, _} = eval(Body, let_binds(Binds, CurEnv)),
-            {Res, CurEnv};
+            eval(Body, let_binds(Binds, Env));
         #mal_sym{val="do"} ->
-            Do = fun (Expr, {_ , Env}) -> eval(Expr, Env) end,
-            lists:foldl(Do, {H, CurEnv}, AstList);
+            Do = fun (Expr, _) -> eval(Expr, Env) end,
+            lists:foldl(Do, eval(H, Env), Tail);
         #mal_sym{val="if"} ->
             [Cond, Body|Else] = Tail,
-            case cond_to_res(eval(Cond, CurEnv)) of % Shouldn't need to propagate Env here.
-                true  -> eval(Body, CurEnv);
-                false -> if length(Else) > 0 -> eval(hd(Else), CurEnv);
-                            true -> {mal_nil, CurEnv} end
+            case cond_to_res(eval(Cond, Env)) of % Shouldn't need to propagate Env here.
+                true  -> eval(Body, Env);
+                false -> if length(Else) > 0 -> eval(hd(Else), Env);
+                            true -> mal_nil end
             end;
         #mal_sym{val="fn*"} ->
             [Params, Body] = Tail,
-            Fn = fun(Args) -> element(1, eval(Body, fn_binds(Params, Args, CurEnv))) end,
-            {Fn, CurEnv};
+            Fn = fun(Args) -> 
+                         Binds = fn_binds(Params, Args, Env),
+%                         io:format("binds are: ~p~n", [Binds]),
+                         eval(Body, env:bound(Binds))
+                 end,
+            Fn;
         _ ->
-            {#mal_list{elems=[F|Args]}, Env} = eval_ast(AstRec, CurEnv),
-            {F(Args), Env}
+            #mal_list{elems=[F|Args]} = eval_ast(AstRec, Env),
+%            io:format("calling ~p with ~p~n", [F, Args]),
+            F(Args)
     end.
 
 fn_binds([], _, Env) -> Env;
@@ -80,35 +86,31 @@ fn_binds(#mal_list{elems=Params}, Args, Env) ->
 fn_binds(#mal_vec{elems=Params}, Args, Env) ->
     fn_binds(Params, Args, Env);
 fn_binds([Param|Rest], [Arg|Args], Env) ->
-    fn_binds(Rest, Args, env:set(Param, Arg, Env)).
+    fn_binds(Rest, Args, env:with(Param, Arg, Env)).
 
-cond_to_res({V, _}) when V =:= mal_false -> false;
-cond_to_res({V, _}) when V =:= mal_nil -> false;
+cond_to_res(V) when V =:= mal_false -> false;
+cond_to_res(V) when V =:= mal_nil -> false;
 cond_to_res(_) -> true.
 
 let_binds([], Env) -> Env;
 let_binds(#mal_list{elems=Binds}, Env) -> let_binds(Binds, Env);
 let_binds(#mal_vec{elems=Binds}, Env) -> let_binds(Binds, Env);
 let_binds([Sym, Expr|Tail], Env) ->
-    {Val, NextEnv} = eval(Expr, Env),
-    let_binds(Tail, env:set(Sym, Val, NextEnv)).
+    Val = eval(Expr, Env),
+    let_binds(Tail, env:with(Sym, Val, Env)).
 
-eval_list_elem(Ast, {List, Env}) ->
-    {Res, NextEnv} = eval(Ast, Env),
-    {List ++ [Res], NextEnv}.
-eval_map_elem(K, Ast, {Pairs, Env}) ->
-    {Res, NextEnv} = eval(Ast, Env),
-    {maps:put(K, Res, Pairs), NextEnv}.
+eval_list_elem(Ast, {List, Env}) -> {List ++ [eval(Ast, Env)], Env}.
+eval_map_elem(K, Ast, {Pairs, Env}) -> {maps:put(K, eval(Ast, Env), Pairs), Env}.
 
 eval_ast(Ast, Env) when is_record(Ast, mal_list) ->
-    {E, NextEnv} = lists:foldl(fun eval_list_elem/2, {[], Env}, Ast#mal_list.elems),
-    {#mal_list{elems=E}, NextEnv};
+    {Elems, _} = lists:foldl(fun eval_list_elem/2, {[], Env}, Ast#mal_list.elems),
+    #mal_list{elems=Elems};
 eval_ast(Ast, Env) when is_record(Ast, mal_vec) ->
-    {E, NextEnv} = lists:foldl(fun eval_list_elem/2, {[], Env}, Ast#mal_vec.elems),
-    {#mal_vec{elems=E}, NextEnv};
+    {Elems, _} = lists:foldl(fun eval_list_elem/2, {[], Env}, Ast#mal_vec.elems),
+    #mal_vec{elems=Elems};
 eval_ast(Ast, Env) when is_record(Ast, mal_map) ->
-    {P, NextEnv} = maps:fold(fun eval_map_elem/3, {#{}, Env}, Ast#mal_map.pairs),
-    {#mal_map{pairs=P}, NextEnv};
+    {Pairs, _} = maps:fold(fun eval_map_elem/3, {#{}, Env}, Ast#mal_map.pairs),
+    #mal_map{pairs=Pairs};
 eval_ast(Ast, Env) when is_record(Ast, mal_sym) ->
-    {env:get(Ast, Env), Env};
-eval_ast(Ast, Env) -> {Ast, Env}.
+    env:get(Ast, Env);
+eval_ast(Ast, _) -> Ast.
