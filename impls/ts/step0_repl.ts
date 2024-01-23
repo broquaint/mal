4da7ebe5bd361b3fc2f2627417c1739f63d4902a
Dan Brook <dan@broquaint.com>
2024-01-23 13:19:51 +0700

    Passing step0_repl for a TypeScript implementation
    Now with deno!

--- impls/ts/step0_repl.ts
+++ impls/ts/step0_repl.ts
@@ -1,35 +1,18 @@
-import { readline } from "./node_readline";
-
-// READ
-function read(str: string): any {
-    // TODO
-    return str;
+function READ(s: string): string {
+    return s
+}
+function EVAL(s: string): string {
+    return s
+}
+function PRINT(s: string): string {
+    return s
+}
+function rep(s: string): string {
+    return PRINT(EVAL(READ(s)))
 }
 
-// EVAL
-function evalMal(ast: any, _env?: any): any {
-    // TODO
-    return ast;
-}
-
-// PRINT
-function print(exp: any): string {
-    // TODO
-    return exp;
-}
-
-function rep(str: string): string {
-    // TODO
-    return print(evalMal(read(str)));
-}
-
-while (true) {
-    const line = readline("user> ");
-    if (line == null) {
-        break;
-    }
-    if (line === "") {
-        continue;
-    }
-    console.log(rep(line));
+let line = prompt('user>')
+while (line !== null) {
+  console.log(rep(line));
+  line = prompt('user>')
 }
